c50c6d2036a3b66e1a74f12acc93f6a509e2e903
Masud Rana <md.masudrana.developer@gmail.com>
2026-01-30 20:53:47 +0800

    Update project

---
+++ task2.js
@@ -0,0 +1,11 @@
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var bangla = 99.50;
+
+var total = math + bio + chem + phy + bangla;
+
+var avarege = total / 5;
+
+console.log(avarege.toFixed(2));
