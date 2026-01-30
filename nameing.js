29b48d7b6a9b91e4621cf6f9ff399bef9263091c
Masud Rana <md.masudrana.developer@gmail.com>
2026-01-30 15:21:50 +0800

    Update project

--- nameing.js
+++ nameing.js
@@ -10,5 +10,25 @@ var is happy = false ;
 var ishappy = true ;
 
 // 3. no quote 
+var "address" = "this wrong";
 
-var "address" = 
+// 4. can not srat with a numbar. but number other than first letter is allowed
+
+var money = 45;
+var money2 = 55;
+var 3money = 88;
+
+// 5. name is case sensitive
+
+address
+Address 
+ADDRESS
+addeRess
+Address
+
+// 6. how to write a long veriable name 
+
+var my corrent home addres = "this is not working"
+var my corrent_home_addres = "this is working"    //snake case 
+var My CorrentHomeAddres = "this is not working"   // camal case
+var my corrent home addres = "this is not working"
