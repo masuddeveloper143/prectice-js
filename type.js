b6a846979efd79cf1f7df63382695a14d3244ebf
Masud Rana <md.masudrana.developer@gmail.com>
2026-01-30 12:04:33 +0800

    Update project

--- type.js
+++ type.js
@@ -1,3 +1,24 @@
 var name = "hello wold";
+var address = "this is string tipe address";
+var contry = "bangladesh";
 
-console.log(name);
+
+
+
+// Boolean 
+
+var is isrich = true;
+
+var passed = false;
+
+
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof contry);
+
+
+
+// Number
+
+var price = 555;
+console.log(typeof price)
