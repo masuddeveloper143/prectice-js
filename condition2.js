0367b89f8796c0cea4d7a664a919283f053b9bd0
Masud Rana <md.masudrana.developer@gmail.com>
2026-01-30 22:48:16 +0800

    Update project

--- condition2.js
+++ condition2.js
@@ -1,5 +1,28 @@
 var weight = 70;
 var height = 1.75;
 
-var bim = weight / (height * height);
+var bmi = weight / (height * height);
+
+bmi = bmi.toFixed(2);
+
+
+var category;
+
+if (bmi < 18.5) {
+
+    category = "underweight";
+
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+
+    category = "Normal";
+} else if (bmi >= 25 && bmi <= 29.9) {
+
+    category = "overheight";
+} else {
+    category = "obse";
+}
+
+console.log("Your BMI is: " + bmi);
+console.log("Health Category: " + category);
+
 
