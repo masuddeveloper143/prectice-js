216cf9d9e85b23736d753d1754e01b550bf34a6c
Masud Rana <md.masudrana.developer@gmail.com>
2026-01-31 11:39:54 +0800

    Update project

---
+++ condition4.js
@@ -0,0 +1,19 @@
+var myscore = 85;
+var frindscore = 65;
+
+if (myscore > 80) {
+
+
+    if (frindscore > 80) {
+        console.log("Good for lunch");
+
+    } else if (frindscore >= 60) {
+        console.log("Good luck next time 👍")
+    } else if (frindscore >= 40) {
+        console.log("Keep friend's message unseen 😶")
+    } i
+
+}
+
+
+
