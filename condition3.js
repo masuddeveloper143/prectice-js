b43e9c58dbd78966cc7ebc6a173408d353d10528
Masud Rana <md.masudrana.developer@gmail.com>
2026-01-31 10:54:54 +0800

    Update project

---
+++ condition3.js
@@ -0,0 +1,25 @@
+var score = 85;
+
+var grade;
+
+if (score >= 90 && score <= 100) {
+    grade = "A";
+}
+
+else if (score >= 80 && score <= 89) {
+    grade = "B";
+}
+
+else if (score >= 70 && score <= 79) {
+    grade = "C";
+}
+
+else if (score >= 60 && score <= 79) {
+    grade = "D";
+}
+
+else {
+    grade = "F";
+}
+
+console.log("grade: ", grade);
