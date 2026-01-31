d692034110751bce10c692932b50d94213b58181
Masud Rana <md.masudrana.developer@gmail.com>
2026-01-31 17:46:46 +0800

    Update project

---
+++ array.js
@@ -0,0 +1,43 @@
+// var fruits = ["banana", "apple", "orenge"]
+
+// fruits.push("papaya") //add last item
+// fruits.pop()  //remove item
+// fruits.unshift("lemon") //add first item
+// // fruits.shift()   //first item remove
+
+
+// console.log(fruits)
+
+
+
+
+
+
+// number array
+
+// var number = [1, 2, 3, 4, 5,]
+
+
+// number.push(6) //add numbetr
+// number.pop() //remove number
+// console.log(number);
+
+
+
+// array inex showing
+
+//
+
+
+// array length in this veriable total how many item count
+
+// var fruits = ["banana", "apple", "orenge", "papaya"]
+
+// console.log(fruits.length)
+
+
+
+
+
+
+
