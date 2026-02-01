5cc60df233d6bfa2876570fbd9431520b7579cf4
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-01 23:40:48 +0800

    Update project

---
+++ comparison.js
@@ -0,0 +1,65 @@
+// console.log(5 > 10);   //flase
+// console.log(5 < 10);m   true
+// console.log(5 == 10);   //flase
+// console.log(10 == 10);  //true
+// console.log(15 >= 10);  //true
+// console.log(5 <= 10);  //true
+// console.log(5 <= 10);  //true
+// console.log(100 <= 10); //false
+// console.log(10 == 10);  //true
+// console.log(11 == 10); //flase
+// console.log(9 == 10);   //flase
+
+
+
+// -------------------------------------------------------
+
+
+// var appePrice = 500;
+// var bananaPrice = 300;
+
+// console.log(appePrice > bananaPrice);
+// console.log(appePrice < bananaPrice);
+// console.log(appePrice >= bananaPrice);
+// console.log(appePrice <= bananaPrice);
+
+
+
+
+
+
+// var age = 12;
+
+
+// if(age > 10){
+//     console.log(you can go);
+// }else{
+//     console.log(you cannot go);
+// }
+
+
+
+
+// var age 15;
+
+// if(age >= 18){
+//     console.log(yes you can go any where);
+// }else if (age >= 13){
+//     console.log(you can go some where);
+// }else{
+//     console.log(cannot go)
+// }
+
+
+var age = 15;
+var withAdult = true;
+
+if (age >= 18) {
+    console.log("can go alone");
+} else {
+    if (withAdult) {
+        console.log("can go adult");
+    } else {
+        console.log("cannot go");
+    }
+}
