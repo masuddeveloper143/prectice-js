3ace74cfff0332e52874df0cd8427a9ad362be43
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-02 01:25:07 +0800

    Update project

---
+++ if.js
@@ -0,0 +1,62 @@
+const age = 17;
+const salary = 50001;
+const isBCS = true;
+const height = 5;
+const hasCar = false;
+
+
+
+
+//  const age = 17;
+
+
+// if (age >= 18) {
+//     console.log("can you give vote");
+// } 
+// else {
+//     console.log("cannot give vot");
+// }
+
+
+
+// if(salary >= 50000 && height >= 6 && isBCS == true){
+//     console.log("selected");
+// }else{
+//     console.log("unselected");
+// }
+
+
+
+// if(salary > 50000 || height > 5){
+//     console.log("selected");
+// }else{
+//     console.log("unselected");
+// }
+
+
+
+// if(salary > 50000 || height > 5  || isBCS == true){
+//     console.log("selected");
+// }else{
+//     console.log("unselected");
+// }
+
+
+
+// ---------------------COMPLEX CONDITION------------
+
+
+
+// if ((salary > 50000 && hasCar == true) || isBCS == true){
+//     console.log("selected");
+// }else{
+//     console.log("unselected");
+// }
+
+
+
+// if ((salary > 50000 || hasCar == true) && isBCS == true) {
+//     console.log("selected");
+// } else {
+//     console.log("unselected");
+// }
