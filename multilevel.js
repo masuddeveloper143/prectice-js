c25e2a41b171501cfe2776bece7c66fa139e9d64
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-02 01:52:37 +0800

    Update project

---
+++ multilevel.js
@@ -0,0 +1,18 @@
+
+
+
+// MALTU LEVEL CONDITION 
+
+
+
+const price = 7000;
+
+if (price >= 5000) {
+    // 10% discount
+    const discount = price * 10 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else{
+    console.log(price);
+}
