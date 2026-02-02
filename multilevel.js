2ff839410df5020b02e5dab4dd8239694c81bbcb
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-02 10:58:42 +0800

    Update project

--- multilevel.js
+++ multilevel.js
@@ -14,7 +14,7 @@ if (price >= 5000) {
     console.log(payAmount);
 }
 else if (price > 2500) {
-    // 10% discount
+    // 5% discount
 
     const discount = price * 5 / 100;
     const payAmount =price - discount;
--- nasted.js
+++ nasted.js
@@ -1,20 +1,22 @@
-const money = 301;
+const money = 0;
 
 if (money > 300) {
     console.log("bro you are rich man!!");
 }
 else {
     if (money > 100) {
-        console.log("bro you are middle rich man!!")
+        console.log("bro you are middle rich man!!");
     }
 
-else{
-    if(money > 0){
-        console.logI("bro you are poor man!!")
+    else {
+        if (money > 1) {
+            console.log("bro you are poor man!!");
+        }
+
+        else {
+            console.log("bro why did not income money? what is the problem");
+        }
     }
 }
-}
 
-else{
-    console.log("bro why did not income money? what is the resson")
-}
+
