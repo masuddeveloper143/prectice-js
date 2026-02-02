f63855505bd1b3a9f6690fcea2fffa4b2cdbf011
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-02 12:44:13 +0800

    Update project

--- ternary.js
+++ ternary.js
@@ -3,11 +3,74 @@
  * 
  */
 
-const age = 18;
 
-if(age > 18){
-    console.log("you can vote")
+
+
+
+
+// const age = 118;
+
+
+// if(age >= 18){
+//     console.log("you can vote")
+// }
+// else{
+//     console.log("cannot vote")
+// }
+
+
+// ☝️👇
+
+
+// age >= 18 ? console.log('you can vote') : console.log('cannot vote')  //short verson
+
+
+
+
+
+
+
+// let price = 500;
+// const isLrader = true;
+
+// if(isLrader === true){
+//     price = 0;
+// }
+// else{
+//     price = price + 100;
+// }
+// console.log(price);
+
+
+
+let price = 500;
+const isLeader = true;
+
+
+// price = isLeader === true ? 0 : price + 100;
+
+// console.log(price)
+
+
+
+
+if (isLeader === true) {
+    if (price >= 1000) {
+        price = price / 2;
+    }
+    else {
+        price = 0;
+    }
+
 }
-else{
-    console.log("cannot vote")
+else {
+    price = price + 100;
 }
+
+console.log(price);
+
+
+
+
+
+
