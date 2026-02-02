f58c4c8bcdf504b6a64f7f946bbae511f72f4d9e
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-02 10:42:44 +0800

    Update project

---
+++ nasted.js
@@ -0,0 +1,20 @@
+const money = 301;
+
+if (money > 300) {
+    console.log("bro you are rich man!!");
+}
+else {
+    if (money > 100) {
+        console.log("bro you are middle rich man!!")
+    }
+
+else{
+    if(money > 0){
+        console.logI("bro you are poor man!!")
+    }
+}
+}
+
+else{
+    console.log("bro why did not income money? what is the resson")
+}
