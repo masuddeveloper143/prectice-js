a818394a924c7c62b70f6e01edf05ed50f685024
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-04 01:32:23 +0800

    Update project

--- lunch.js
+++ lunch.js
@@ -1,2 +1,20 @@
-let myScore = 80;
-let frindScore = 65;
+let myScore = 84;
+let frindScore = 81;
+
+if (myScore > 80) {
+
+    if (frindScore > 80) {
+        console.log("go to for a lunch");
+    } else if (frindScore > 60 && frindScore < 79.9) {
+        console.log("good luck next time");
+    } else if (frindScore > 40 && frindScore < 59.9) {
+        console.log("keep your frind messege unseen");
+    }
+    else {
+        console.log("block your frind");
+    }
+}
+
+else {
+    console.log("go to the home then sleep");
+}
---
+++ num1.js
@@ -0,0 +1,18 @@
+let num1 = 10;
+let num2 = 5;
+
+// let result;
+
+// if (num1 > num2 ) {
+//     result = num1 * 2;
+// }
+// else{
+//     result = num1 + num2;
+// }
+
+// console.log(result);
+
+
+let result = num1 > num2 ? num1 * 2 : num1 + num2;
+
+console.log(result);
