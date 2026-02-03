4d35780a8925e9d00faf9b4c9ded04836fe05b02
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-03 22:57:57 +0800

    Update project

---
+++ free.drinks.js
@@ -0,0 +1,22 @@
+// let price = 1000;
+// let cokeprice;
+
+// if (price > 1000) {
+//     cokeprice = 0;
+// }
+
+// else {
+//     console.log(cokeprice = 30);
+// }
+
+// console.log("cokeprice: " + cokeprice + "tk")
+
+
+let price = 1001; 
+
+if(price > 1000){
+    console.log("coke 0tk");
+}else{
+    console.log("coke 30tk");
+}
+    
