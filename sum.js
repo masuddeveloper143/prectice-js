f088d93b239dd8ff06dd39326ecfad57a5d311fc
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-04 10:28:33 +0800

    Update project

---
+++ sum.js
@@ -0,0 +1,3 @@
+let num = 1;
+let sum = 0;
+
--- while_loop.js
+++ while_loop.js
@@ -1,6 +1,20 @@
 let num = 1;
 
-while(num < 11){
+while (num <= 10) {
     console.log(num);
     num++;
 }
+
+console.log("next step strat");
+
+
+
+
+let num1 = 50;
+
+while (num1 <= 100) {
+    console.log(num1);
+    num1++;
+}
+
+console.log("next step all done");
