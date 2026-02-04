14b443050ce3fd22067ed68f4ccb7931fbfe0c83
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-04 17:20:32 +0800

    Update project

---
+++ incrementelloop.js
@@ -0,0 +1,68 @@
+// for (i = 1 ; i <= 10; i++){
+//     console.log(i)
+// }
+
+//dikrimental for loop
+
+// for (i = 0; i <= 50; i--) {
+//     console.log(i);
+// }
+for (i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
