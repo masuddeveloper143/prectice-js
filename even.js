391fb008fc9320dce7556c16f54fd2b2d64cdb5b
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-04 11:32:23 +0800

    Update project

--- even.js
+++ even.js
@@ -1,9 +1,10 @@
-let num = 1;
+// let num = 1;
 
-while (num <= 10) {
-    console.log(num);
-    if (num % 2 === 0) {
-        console.log("even number", num);
-        num++;
-    }
-} 
+// while (num <= 10) {
+//     console.log(num);
+//     if (num % 2 === 0) {
+//         console.log('even number', num);
+
+//     }
+//     num++;   //num++ while loop-এর শেষে রাখতে হবে, যেন সব ক্ষেত্রে চলে
+// } 
