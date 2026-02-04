0417a680f77793f10cb8b563dc5bbc0c52a6d526
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-04 20:15:34 +0800

    Update project

--- contineue.js
+++ contineue.js
@@ -1,6 +1,16 @@
-for (i = 1; i < 10; i++) {
-    if (i % 2 === 1) {
+// for (i = 1; i < 10; i++) {
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+let n = 0;
+
+while (n < 50) (
+    n++;
+    if (n % 5 !== 0){
         continue;
     }
-    console.log(i);
-}
+    console.log(n)
+)
