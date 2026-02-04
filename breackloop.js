b8bdf198e0b799ab5ead57fed57e59509a1d7383
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-04 20:09:22 +0800

    Update project

---
+++ breackloop.js
@@ -0,0 +1,21 @@
+for (let i = 1; i <= 150; i++) {
+    console.log(i);
+    if (i >= 30) {
+        break;
+    }
+}
+
+console.log('life after break up!');
+
+let n = 54;
+while (n > 25) {
+    console.log(n);
+    if (n < 50) {
+        break;
+    }
+    n--;
+}
+
+
+
+
--- even.js
+++ even.js
@@ -12,8 +12,46 @@
 
 
 
-for (let i = 0; i < 20; i++){
-    if(i %2 !==0){
+// for (let i = 0; i < 20; i++){
+//     if(i %2 ===0){
+//         console.log(i);
+//     }
+// }
+// for (let i = 1; i < 20; i+=2){
+//     if(i %2 !==0){
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 1; i <= 40; i++) {
+//     if (i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+// for (let i = 1; i <= 40; i++) {
+//     if (i % 3 === 0 || i % 4 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+// for (let i = 1; i <= 40; i++) {
+//     if (i % 3 === 0 && i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+let total = 0;
+for (let i = 1; i <= 43; i++) {
+    if (i % 2 !== 0) {
         console.log(i);
-    }
+        total = total + i;
+        console.log("total: ", total);
 }
+
+}
+console.log(total);
+
+
