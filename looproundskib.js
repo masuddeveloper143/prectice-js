4d255957f05c12f5de100b97319017393d0e5c94
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-04 12:38:54 +0800

    Update project

--- looproundskib.js
+++ looproundskib.js
@@ -1,7 +1,10 @@
-let num = 1; 
+let num = 1;
 
-while(num <= 40){
-    if(num ){
-
+while (num <= 40) {
+    if (num % 2 !== 0) {
+        num++;
+        continue;
     }
+    console.log(num);
+    num++;
 }
