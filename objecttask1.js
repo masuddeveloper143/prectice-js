d93292fcc44835f55107c5d49a419d93f76f84f9
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-05 11:41:43 +0800

    Update project

--- objecttask1.js
+++ objecttask1.js
@@ -9,9 +9,27 @@
 
 // For this object below add a property named passenger capacity with value 5
 
-let car = {
-    make: "Toyota",
-    model: "Corolla",
-    year: 2020
-};;
-console.log(car[3]);
+// let car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+
+// car["messenjar opacty"] = 5
+// console.log(car);
+
+let student = {
+    name: 'hakim uddin',
+    id: 44,
+    
+    physics:{
+        subject: 'HSC physics',
+        anoter: 'hakim alam',
+        marks: 30
+
+    }
+
+}
+
+console.log(student.physics.marks)
+ 
