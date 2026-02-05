13ba9d24d4a59755f3cba80c5bae7275a1bf9506
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-05 16:40:22 +0800

    Update project

--- string.js
+++ string.js
@@ -1,8 +1,15 @@
-let text = "banana";
-let count = 0;
 
-for (let i = 0; i < text.length; i++) {
-    if (text[i] === 'a') {
-        count++;
-    }
-}
+//Count how many times a string has the letter a
+
+// let text = "banana";
+// let count = 0;
+
+// for (let i = 0; i < text.length; i++) {
+//     if (text[i] === 'a') {
+//         count++;
+//     }
+// }
+
+// console.log(count);
+
+
