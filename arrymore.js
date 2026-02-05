205b273458fa5b792b1cb993998522e73206f914
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-06 00:53:56 +0800

    Update project

--- arrymore.js
+++ arrymore.js
@@ -1,5 +1,47 @@
-const friends = ['elon', 'bill', 'mark', 'wlen'];
 
-for(const friend of friends){
-    console.log(friend)
+
+//---------------------------------------------------------------------
+
+// const friends = ['elon', 'bill', 'mark', 'wlen'];
+
+// for (const friend of friends) {
+//     // console.log(friend);
+
+// }
+
+// console.log(friends.length);
+// console.log(friends[2]);
+
+
+
+
+
+
+//-------------------------------------------------------------------
+// const friends = ['elon', 'bill', 'mark', 'wlen'];
+
+// for (const friend of friends) {
+
+// }
+// for (let i = 0; i <= 50; i++) {
+//     console.log(i);
+//     console.log(friends[i]);
+// }
+// for (let i = 0; i < friends.length; i++) {
+//     console.log(i);
+//     console.log(friends[i]);
+// }
+
+
+
+//----------------------------------------------------------------
+
+
+const number = ['7', '16', '15', '55', '8', '55',]
+
+for (let i = 0; i < number.length; i++) {
+
+    console.log(i);
+    console.log(number[i]);
+
 }
