66b62803331816bcff1e4be9fc1f50ac48aeb24b
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-05 11:09:00 +0800

    Update project

--- objecttask1.js
+++ objecttask1.js
@@ -1,8 +1,17 @@
-const colors = {
-    red: "#ff0000",
-    green: "#00ff00",
-    blue: "#0000ff",
-    "golden rod": '#daa520'
-};
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// };
 
-console.log(colors["golden rod"]);
+// console.log(colors["golden rod"]);
+
+// For this object below add a property named passenger capacity with value 5
+
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};;
+console.log(car[3]);
