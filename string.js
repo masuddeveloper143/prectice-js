2c3958220fb887b02057455686ebe591207bcad9
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-05 16:46:55 +0800

    Update project

--- string.js
+++ string.js
@@ -13,3 +13,15 @@
 // console.log(count);
 
 
+// let text = "AnAppleAndBanana";
+// let count = 0;
+
+// for (let i = 0; i < text.length; i++) {
+//     if (text[i] === 'a' || text[i] === 'A') {
+//         count++;
+//     }
+// }
+
+// console.log(count);
+
+
