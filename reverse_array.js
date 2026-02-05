8aa1b224867fc5ec33aab2fdaba556cfe434c759
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-06 01:29:15 +0800

    Update project

--- reverse_array.js
+++ reverse_array.js
@@ -1,3 +1,22 @@
-const number = ['1', '2', '3', '4',];
+// const number = ['1', '2', '3', '4',];
 
-console.log(number.reverse());
+// console.log(number);
+
+// console.log(number.reverse())
+
+//---------------------------------------------------------------------------------
+
+// const number = ['1', '2', '3', '4',];
+
+// console.log(number.reverse())
+
+
+//------------------------------------------------------
+
+// const number = ['1', '2', '3', '4',];
+
+// const reversed = number.reverse();
+
+// console.log(reversed);
+
+//----------------------------------------------------
