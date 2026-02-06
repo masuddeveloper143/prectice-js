29ea45e2b6c3c64390d8a320e84851ef2b91feb1
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-07 01:07:30 +0800

    Update project

--- student.array.js
+++ student.array.js
@@ -1,1 +1,9 @@
-let students = 
+let students = [
+    { name: "Rahim", marks: 85 },
+    { name: "karim", marks: 72 },
+    { name: "sumi", marks: 90 }
+];
+
+for (i = 0; i < students.length; i++) {
+    console.log("name: ", students[i].name, " | marks: ", students[i].marks);
+}
