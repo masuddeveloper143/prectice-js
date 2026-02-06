5a5eca4b325c29e77ae403dd04809217cf4fc658
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-07 00:41:13 +0800

    Update project

---
+++ array.copy.js
@@ -0,0 +1,8 @@
+let orginal = [20, 44, 55, 77, 66,];
+
+let copy = [...orginal];
+
+copy[0] = 99;
+
+console.log("orginal: ", orginal);
+console.log("copy", copy);
--- arraylooping4.js
+++ arraylooping4.js
@@ -5,7 +5,7 @@ let words = sentence.split("");
 let reversedWord = [];
 
 for (i = words.length - 1; i >= 0; i--) {
-    reversedWord.push[i];
+    reversedWord.push(words[i]);
 }
 
 let result = reversedWord.join(" ");
