7802143203e826345e620c95b66e43cc2698971f
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-07 00:22:40 +0800

    Update project

---
+++ arraylooping4.js
@@ -0,0 +1,12 @@
+let sentence = "I am learning javascript";
+
+let words = sentence.split("");
+
+let reversedWord = [];
+
+for (i = words.length - 1; i >= 0; i--) {
+    reversedWord.push[i];
+}
+
+let result = reversedWord.join(" ");
+console.log(result);
