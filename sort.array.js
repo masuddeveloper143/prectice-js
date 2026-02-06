9ebf4a4fe50062f63f68113e168a49a2840c75c7
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-06 14:21:33 +0800

    Update project

--- sort.array.js
+++ sort.array.js
@@ -1,2 +1,8 @@
 const person = ['masud', 'romjan', 'rubel', 'naiyeem', 'forhad'];
 
+const sortedPerson = person.sort();
+console.log(sortedPerson);
+
+// sort 
+
+const numbers = [3, 8, 9, 2, 6, 3,];
