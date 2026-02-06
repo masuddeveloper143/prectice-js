f3e71a8a9bfe060dd41a6d332603d352a719bf1f
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-06 14:46:14 +0800

    Update project

--- sort.array.js
+++ sort.array.js
@@ -1,3 +1,23 @@
+// const person = ['masud', 'romjan', 'rubel', 'naiyeem', 'forhad'];
+
+// const sortedPerson = person.sort();
+// console.log(sortedPerson);
+
+// // sort 
+
+// const numbers = [3, 8, 9, 2, 6, 5,];
+
+// // ascending smaller to larger  ---> [2, 3, 6, 8, 9,] 
+
+
+// // dscending smaller to larger  ---> [9, 8, 6, 3, 2,] 
+
+// const numbers_asc = numbers.sort();
+// console.log(numbers_asc);
+
+
+// ---------------------------------------------------------------------------------
+
 const person = ['masud', 'romjan', 'rubel', 'naiyeem', 'forhad'];
 
 const sortedPerson = person.sort();
@@ -5,4 +25,17 @@ console.log(sortedPerson);
 
 // sort 
 
-const numbers = [3, 8, 9, 2, 6, 3,];
+const numbers = [4, 7, 9, 12, 8, 43, 6, 1];
+
+// ascending smaller to larger  ---> [2, 3, 6, 8, 9,] 
+
+
+// dscending smaller to larger  ---> [9, 8, 6, 3, 2,] 
+
+// const numbers_asc = numbers.sort(); //not working 
+const numbers_asc = [...numbers].sort(function (a, b) { return a - b })
+const numbers_dcs = [...numbers].sort(function (a, b) { return a - b })
+
+console.log(numbers_asc);
+console.log(numbers_dcs);
+
