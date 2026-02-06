5e9af7d453daf2148a5f5bfad72c7ee9ca7d7f80
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-06 15:41:41 +0800

    Update project

--- revers_task1.js
+++ revers_task1.js
@@ -1,3 +1,25 @@
-const colors = ['orange', 'red', 'green', 'yellow', 'blue'];
+// this is normal revers system 👇
+// const colors = ['orange', 'red', 'green', 'yellow', 'blue'];
 
-console.log(colors.reverse());
+// console.log(colors.reverse());
+
+// --------------------------------------------------------------------------------------------------------------------------------------
+
+// Write a JavaScript code to reverse the array colors without using the reverse method.
+
+// this is without revers
+// this is for loop👇
+
+// let colors = ['orange', 'red', 'green', 'yellow', 'blue'];
+
+// let reversedColors = [];
+
+// for (let i = colors.length - 1; i >= 0; i--) {
+//     reversedColors.push(colors[i]);
+// }
+
+// console.log(reversedColors);
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
