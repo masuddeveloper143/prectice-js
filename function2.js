e7e056152528ceb175ebe5d0bad6defa99308cac
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-08 00:59:08 +0800

    Update project

---
+++ function2.js
@@ -0,0 +1,16 @@
+function doubelIt (nember){
+    const doubled = nember * number;
+    console.log(number, doubled);
+}
+
+console.log('i will calle the function');
+
+doubelIt(3);
+console.log('-----------------------------------');
+doubelIt(8);
+console.log('-----------------------------------');
+doubelIt(9);
+console.log('-----------------------------------');
+doubelIt(8);
+
+
