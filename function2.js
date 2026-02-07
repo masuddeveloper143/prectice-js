021b3ad0f9488b3ce3174c95efe0d6d869648fbd
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-08 01:18:23 +0800

    Update project

--- function2.js
+++ function2.js
@@ -1,16 +1,29 @@
-function doubelIt (nember){
-    const doubled = nember * number;
+function doubelIt(number) {
+    const doubled = number * 2;
     console.log(number, doubled);
 }
 
-console.log('i will calle the function');
+console.log('i will calle the function')
 
+doubelIt(1);
+console.log('-----------------------------------');
+doubelIt(2);
+console.log('-----------------------------------');
 doubelIt(3);
 console.log('-----------------------------------');
+doubelIt(4);
+console.log('-----------------------------------');
+doubelIt(5);
+console.log('-----------------------------------');
+doubelIt(6);
+console.log('-----------------------------------');
+doubelIt(7);
+console.log('-----------------------------------');
 doubelIt(8);
 console.log('-----------------------------------');
-doubelIt(9);
-console.log('-----------------------------------');
-doubelIt(8);
 
+const number = 55;
+doubelIt(number);
 
+const money = 112;
+doubelIt(money);
