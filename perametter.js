f3ab1dd4aaa558f00dad8caa82b9cde0c5a014eb
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-08 00:11:33 +0800

    Update project

--- perametter.js
+++ perametter.js
@@ -1,25 +1,33 @@
-function lerning() {
-    console.log('i wan to lerning an english');
-    console.log('but how to i will lern an english');
-    console.log('i do not know');
-    console.log('is there anyone like taht tich me an english')
+// function lerning() {
+//     console.log('i wan to lerning an english');
+//     console.log('but how to i will lern an english');
+//     console.log('i do not know');
+//     console.log('is there anyone like taht tich me an english')
 
+// }
+
+//  lerning();
+
+//  --------------------------------------------------------------------------------------------------------------
+
+// function square(number) {
+//     console.log(number);
+//     console.log('value of the number perametter', number);
+//     const borgo = number * number;
+//     console.log("square of the given number is: ", borgo);
+
+// }
+// square(4);
+// console.log('--------------------------------')
+// square(3);
+// square(5);
+// square(6);
+// square(55);
+// square(13);
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+function add(num1, num2){
+c
 }
-
-// lerning();
-
-function square(number) {
-    console.log(number)
-    console.log('value of the number perametter', number);
-    const borgo = number * number;
-    console.log("square of the given number is: ", borgo);
-
-}
-square(4);
-console.log('--------------------------------');
-square(3);
-square(5);
-square(6);
-square(55);
-square(13);
 
