916db8c91ff08ceb5759128338dd19fbfbf30056
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-07 23:39:59 +0800

    Update project

---
+++ perametter.js
@@ -0,0 +1,13 @@
+function lerning() {
+    console.log('i wan to lerning an english');
+    console.log('but how to i will lern an english');
+    console.log('i do not know');
+    console.log('is there anyone like taht tich me an english')
+
+}
+
+// lerning();
+
+function square (numbers){
+    console.log(numbers)
+}
