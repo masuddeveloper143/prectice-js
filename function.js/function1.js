6848993daf5d22baae2b4780b7072a5cf2847b93
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-07 14:51:28 +0800

    Update project

---
+++ function.js/function1.js
@@ -0,0 +1,27 @@
+const numbers = 45;
+
+function fanOffkor() {
+    console.log('bosha theke ute dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off of the fan');
+
+}
+
+
+// calle the function system 👇
+fanOffkor();
+
+// function diclear system👇
+
+// function name(){
+
+// }
+
+function brushTeeth() {
+    console.log('pic up the brush');
+    console.log('add paste');
+    console.log('2 min goso and goso fena felo');
+}
+brushTeeth();
+
+
