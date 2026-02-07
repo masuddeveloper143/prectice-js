e5f3d5ddadad7c11a2df706684314749c1f7028c
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-08 00:30:10 +0800

    Update project

--- perametter.js
+++ perametter.js
@@ -27,10 +27,21 @@
 
 // ---------------------------------------------------------------------------------------------------------------------------
 
-function add(num1, num2){
-const sum = num1 + num2;
-console.log(sum);
+// function add(num1, num2){
+// const sum = num1 + num2;
+// console.log(sum);
+// }
+
+// add (5, 71);
+// add(4, 73);
+// add(88, 77);
+
+// ------------------------------------------------------------------------------------------------------
+
+function addAll(a, b, c, d, e,) {
+    const total = a + b + c + d + e;
+    console.log("total numbers: ", total);
 }
 
-add (5, 71);
+addAll(1, 2, 3, 4, 5,);
 
