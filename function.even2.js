7f9602f1328a7ffac372795192306afbafd889cb
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-09 00:57:51 +0800

    Update project

--- function.even2.js
+++ function.even2.js
@@ -3,4 +3,13 @@
 
 // create a function that wo=ill return only even numbetrs return the sum of even number 
 
+function evenNumbersOnly(numbers) {
+    let sum = 0;
+    console.log(numbers)
+    sum = sum + numbers;
+}
 
+
+const result = [4, 4, 5, 7, 4, 3,]
+evenNumbersOnly(result);
+console.log('this is the finel number: ', sum);
