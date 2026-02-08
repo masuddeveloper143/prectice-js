fc3741e333614c0e9760c7cad774a451d9ee7469
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-08 10:34:50 +0800

    Update project

--- function3.js
+++ function3.js
@@ -22,9 +22,12 @@
 // ---------------------------------------------------------------------------------------------------
 
 function doMath(num1, num2) {
-    const sum = n(perameter) num1: any
+    const sum = num1 + num2;
     const diff = num1 - num2;
     const multiply = sum * diff;
     const result = multiply / 2;
     return result;
 }
+
+const result = doMath(50, 10);
+console.log("output: ", result);
