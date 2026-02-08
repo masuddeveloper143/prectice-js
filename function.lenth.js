bd542b85b7aa966197a35da253700d1826a79e8b
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-08 17:03:01 +0800

    Update project

--- function.lenth.js
+++ function.lenth.js
@@ -1,4 +1,16 @@
 function evenSizedString(str) {
     const size = str.length;
     console.log(str, size);
+
+    if(size % 2 === 0){
+    console.log('even size')
+    return true;
+
 }
+
+else{
+    console.log('odd size')
+    return true;
+}
+evenSizedString('dhaka');
+evenSizedString('faka');
