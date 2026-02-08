7af406b0a7fdf590828dfcf37dc9eeae534db142
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-08 08:46:00 +0800

    Update project

--- function2.js
+++ function2.js
@@ -1,32 +1,45 @@
-function doubelIt(number) {
-    const doubled = number * 2;
-    console.log(number, doubled);
-}
+// function doubelIt(number) {
+//     const doubled = number * 2;
+//     console.log(number, doubled);
+// }
 
-console.log('i will calle the function')
+// console.log('i will calle the function')
 
-doubelIt(1);
-console.log('-----------------------------------');
-doubelIt(2);
-console.log('-----------------------------------');
-doubelIt(3);
-console.log('-----------------------------------');
-doubelIt(4);
-console.log('-----------------------------------');
-doubelIt(5);
-console.log('-----------------------------------');
-doubelIt(6);
-console.log('-----------------------------------');
-doubelIt(7);
-console.log('-----------------------------------');
-doubelIt(8);
-console.log('-----------------------------------');
+// doubelIt(1);
+// console.log('-----------------------------------');
+// doubelIt(2);
+// console.log('-----------------------------------');
+// doubelIt(3);
+// console.log('-----------------------------------');
+// doubelIt(4);
+// console.log('-----------------------------------');
+// doubelIt(5);
+// console.log('-----------------------------------');
+// doubelIt(6);
+// console.log('-----------------------------------');
+// doubelIt(7);
+// console.log('-----------------------------------');
+// doubelIt(8);
+// console.log('-----------------------------------');
 
-const number = 55;
-doubelIt(number);
+// const number = 55;
+// doubelIt(number);
 
-const money = 112;
-doubelIt(money);
+// const money = 112;
+// doubelIt(money);
 
 // --------------------------------------------------------------------------------------
+
+function diffetence(num1, num2) {
+    const diff = num1 - num2;
+    console.log(num1, num2, "difarence is: " ,  diff);
+}
+
+const ftherAge = 40;
+
+const myAge = 10;
+
+diffetence(ftherAge, myAge);
+// ------------------------------------------------------------------------------------------
+
 
