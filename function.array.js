9cc11579b47f56e6f1c10ca978a9f4eedbefcb21
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-09 00:20:36 +0800

    Update project

---
+++ function.array.js
@@ -0,0 +1,34 @@
+
+
+// ovjective : write a function give me the sum of numbers in an array 
+// step1: diclear a function 
+// step 2 : call chack whether the funcrion is called porperly 
+// step 3 : set a perametar(s)
+// step 4 : pass the perametar (s) chack the whether parametar passed in a proper formet
+
+
+
+// function sumOfNumber(numbers) {
+//     console.log(numbers)
+// }
+
+// const numbs = [23, 44, 5, 4,]
+// const sum = sumOfNumber(numbs);
+// console.log('sum of number is');
+
+
+// -----------------------------------------------------------------------------------------
+
+// step 5: do the function task step by step
+
+
+function sumOfNumber(numbers) {
+    
+    for(const number of numbers){
+        console.log(number);
+    }
+}
+
+const numbs = [23, 44, 5, 4,]
+const sum = sumOfNumber(numbs);
+console.log('sum of number is');
