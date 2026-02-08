df96538f2c156696b835f8d23462a64023684b2a
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-08 18:48:38 +0800

    Update project

--- function.lenth.js
+++ function.lenth.js
@@ -1,16 +1,63 @@
-function evenSizedString(str) {
-    const size = str.length;
-    console.log(str, size);
 
-    if(size % 2 === 0){
-    console.log('even size')
-    return true;
 
+// // this is function length even odd Number fund it 
+
+// function evenSizedString(str) {
+//     const size = str.length;
+//     console.log(str, size);
+
+//     if (size % 2 === 0) {
+//         console.log('even size');
+//         return true;
+
+//     }
+
+
+//     else {
+//         console.log('odd size');
+//         return true;
+//     }
+// }
+
+// evenSizedString('this is the function str value');
+// evenSizedString('this is the function str value');
+
+// --------------------------------------------------------------------------------------
+
+
+// function doubleOrTriple(number, doDouble) {
+//     if (doDouble === true) {
+//         const result = number * 2;
+//         return result;
+//     }
+//     else {
+//         const result = number * 3;
+//         return result;
+//     }
+// }
+
+// console.log(doubleOrTriple(5, true));
+// console.log(doubleOrTriple(5, false));
+
+// --------------------------------------------------------------------------------------------
+
+// function numberOfElement(number) {
+//     const len = number.length;
+//     return len;
+// }
+// const result = numberOfElement([22, 444, 66, 8, 9, 56, 88])
+// console.log(result);
+
+// ---------------------------------------------------------------------------------------------------
+
+function getAge(person) {
+    const age = person.age;
+    return age;
 }
 
-else{
-    console.log('odd size')
-    return true;
-}
-evenSizedString('dhaka');
-evenSizedString('faka');
+const result = getAge(24, person);
+
+console.log(result);
+
+
+
