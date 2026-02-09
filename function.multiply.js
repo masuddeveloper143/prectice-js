abef2abb5d9195db29c6a13c43c7a27f2847d542
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-09 15:44:50 +0800

    Update project

--- function.multiply.js
+++ function.multiply.js
@@ -2,4 +2,12 @@
 
 // Take four parameters. Multiply the four numbers and then return the result
 
-function fourPramrtars(a, b, c, d)
+function fourPramrtars(a, b, c, d) {
+    let result = a * b * c * d;
+    console.log(result);
+}
+
+let output = fourPramrtars(1,2,3,4,);
+console.log(output);
+
+
