3ad7483fae04972d2ad42546434178e03fbfc2ad
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-11 01:26:31 +0800

    Update project

---
+++ leapyear.js
@@ -0,0 +1,35 @@
+
+// semple leapYear
+// year will be leap year if the year is diviseble by 4
+
+// those yeara that is not divisbile by 100, if the year dividible by 4
+
+function leapYear(leap) {
+    if (leap % 4 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+let result = leapYear(2043);
+// console.log(result);
+
+// -----------------------------------------------------------------------------------------------
+
+
+function islearYear2(year) {
+    if (year % 100 !== 0 && year % 4 === 0) {
+        return true;
+    }
+    else if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+let resultt = islearYear2(2043)
+console.log(resultt);
