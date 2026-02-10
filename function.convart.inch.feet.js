4b18f018c2886a93e90b05f27f3361982fc167df
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-10 15:39:20 +0800

    Update project

--- function.convart.inch.feet.js
+++ function.convart.inch.feet.js
@@ -1,9 +1,25 @@
-// 12 inch 1feet
+// 12 inch 1feet only number found normaly
 
-function inchToFeet(inch) {
-    let feet = inch / 12;
-    return feet;
+// function inchToFeet(inch) {
+//     let feet = inch / 12;
+//     return feet;
+// }
+
+// let ShovoAge = inchToFeet((75));
+// console.log(ShovoAge);
+
+// ------------------------------------------------------------
+
+// this is the string part like a (6feet 3inch) 
+
+function inchToFeet2(inch) {
+    let feetFaction = inch / 12;
+    let feetNumber = parseInt(feetFaction);
+    let inchRemaining = inch % 12;
+    let result = feetNumber + ' ft ' + inchRemaining + ' inch '
+    return result;
 }
 
-let ShovoAge = inchToFeet((75));
+let ShovoAge = inchToFeet2(75);
 console.log(ShovoAge);
+
