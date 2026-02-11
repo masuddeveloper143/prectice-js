1500eb34e94a18927755de6049906e58481b7da2
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-11 21:04:18 +0800

    Update project

--- function.odd.average.js
+++ function.odd.average.js
@@ -9,7 +9,8 @@
 // let avg = oddAverage(numbers);
 // console.log('avaerage of the odd number is: ', avg);
 
-// // [-----------------------------------------------------------------------------------------------]
+//[-----------------------------------------------------------------------------------------------]
+// this is function loop 👇
 
 // function oddAverage(numbers) {
 //     for (let number of numbers) {
@@ -24,15 +25,20 @@
 
 // // ----------------------------------------------------------------------------------------
 
+// this is function fund odd numbers 👇
 
 // function oddAverage(numbers) {
 //     for (let number of numbers) {
-//         console.log(number);
+//         if (number % 2 === 1) {
+//             console.log(number);
+//         }
+
 //     }
+
 
 // }
 
-// let numbers = [12, 13, 14, 15];
+// let numbers = [12, 13, 14, 15, 48, 54, 75, 55];
 // let avg = oddAverage(numbers);
 // console.log('avaerage of the odd number is: ', avg);
 
@@ -40,16 +46,20 @@
 
 // ------------------------------------------------------------------------------
 
+// this is the function fund even numbers👇
+
 function oddAverage(numbers) {
-    if (number % 2 ===  1) {
-        console.log(number);
+    for (let number of numbers) {
+        if (number % 2 === 0) {
+            console.log(number)
+        }
     }
-    return number;
 }
 
 
 
-let numbers = [12, 13, 14, 15];
+
+let numbers = [12, 13, 14, 15, 48, 54, 75, 55];
 let avg = oddAverage(numbers);
 console.log('avaerage of the odd number is: ', avg);
 
