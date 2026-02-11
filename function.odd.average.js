ee6601d63faaa6e654cce2c9d2940f535a123cc9
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-11 14:47:51 +0800

    Update project

---
+++ function.odd.average.js
@@ -0,0 +1,68 @@
+// function takes an Array as perametar the array 
+
+// function oddAverage(numbers) {
+//     console.log(numbers);
+
+// }
+
+// let numbers = [12, 13, 14, 15];
+// let avg = oddAverage(numbers);
+// console.log('avaerage of the odd number is: ', avg);
+
+// // [-----------------------------------------------------------------------------------------------]
+
+// function oddAverage(numbers) {
+//     for (let number of numbers) {
+//         console.log(number);
+//     }
+
+// }
+
+// let numbers = [12, 13, 14, 15];
+// let avg = oddAverage(numbers);
+// console.log('avaerage of the odd number is: ', avg);
+
+// // ----------------------------------------------------------------------------------------
+
+
+// function oddAverage(numbers) {
+//     for (let number of numbers) {
+//         console.log(number);
+//     }
+
+// }
+
+// let numbers = [12, 13, 14, 15];
+// let avg = oddAverage(numbers);
+// console.log('avaerage of the odd number is: ', avg);
+
+
+
+// ------------------------------------------------------------------------------
+
+function oddAverage(numbers) {
+    if (number % 2 ===  1) {
+        console.log(number);
+    }
+    return number;
+}
+
+
+
+let numbers = [12, 13, 14, 15];
+let avg = oddAverage(numbers);
+console.log('avaerage of the odd number is: ', avg);
+
+
+// ----------------------------------------------------------------------------------
+
+// function oddAverage(numbers) {
+//     for (let number of numbers) {
+//         console.log(number);
+//     }
+
+// }
+
+// let numbers = [12, 13, 14, 15];
+// let avg = oddAverage(numbers);
+// console.log('avaerage of the odd number is: ', avg);
