bce7b6961f54b8738429c1ccdc407cfe8fd3ff6d
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 00:14:16 +0800

    Update project

--- function.math.js
+++ function.math.js
@@ -13,10 +13,20 @@
 // console.log(max);
 // ------------------------------------------------------------------------------
 
-let max = Math.max(55, 66, 88, 88, 88, 80);
-console.log(max);
+// let max = Math.max(55, 66, 88, 88, 88, 80);
+// console.log(max);
 
-console.log(Math.PI);
+// console.log(Math.PI);
+// console.log(Math.abs(60-9));
+// console.log(Math.round(2.49));
+// console.log(Math.floor(2.99));
+// console.log(Math.ceil(2.04));
+
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+let rand = Math.round(Math.random() * 10)
+console.log(rand);
 // ----------------------------------------------------------------------------------------
 
 
