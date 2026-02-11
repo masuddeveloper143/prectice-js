75c37da5571b71fc4508ca4816020ab68d60dfdf
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-11 21:45:52 +0800

    Update project

--- function.odd.average.js
+++ function.odd.average.js
@@ -1,4 +1,4 @@
-// function takes an Array as perametar the array 
+// function takes an Array as perametar the array
 
 // function oddAverage(numbers) {
 //     console.log(numbers);
@@ -48,31 +48,141 @@
 
 // this is the function fund even numbers👇
 
+// function oddAverage(numbers) {
+//     for (let number of numbers) {
+//         if (number % 2 === 0) {
+//             console.log(number)
+//         }
+//     }
+// }
+
+
+
+
+// let numbers = [12, 13, 14, 15, 48, 54, 75, 55];
+// let avg = oddAverage(numbers);
+// console.log('avaerage of the odd number is: ', avg);
+
+
+// ----------------------------------------------------------------------------------
+
+
+// thi is a function odds number fund for loop odds containers👇
+
+// function oddAverage(numbers) {
+// // odds is the array thhat contains only the odd numbers 
+//     let odds = []
+
+
+//     for (let number of numbers) {
+
+//         if (number % 2 === 0) {
+//             // console.log(number);
+//             odds.push(number);
+//         }
+//     }
+//     console.log(odds);
+//     return odds;
+// }
+
+// let numbers = [12, 13, 14, 15, 48, 54, 75, 55];
+// let avg = oddAverage(numbers);
+// console.log('avaerage of the odd number is: ', avg);
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+// this is a function fund even numbers and even numbers total how many numbers countintg and length fund 👇
+
+// function oddAverage(numbers) {
+
+//     let odds = []
+
+
+//     for (let number of numbers) {
+
+//         if (number % 2 === 0) {
+//             console.log(number);
+//             odds.push(number);
+//         }
+
+
+//     }
+
+//     let sum = 0;
+//     for (number of odds) {
+//         sum = sum + number;
+//     }
+
+//     console.log(sum);
+
+// }
+
+// let numbers = [12, 13, 14, 15, 48, 54, 75, 55];
+// let avg = oddAverage(numbers);
+// console.log('avaerage of the odd number is: ', avg);
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+// this is a function fund even numbers and odd numbers total how many numbers countintg and length fund 👇
+
 function oddAverage(numbers) {
+
+    let odds = []
+
+
     for (let number of numbers) {
-        if (number % 2 === 0) {
-            console.log(number)
+
+        if (number % 2 === 1) {
+            console.log(number);
+            odds.push(number);
         }
+
+
     }
+
+    let sum = 0;
+    for (number of odds) {
+        sum = sum + number;
+    }
+    let count = odds.length;
+    console.log(sum, count);
+    let avg = sum / count;
+    console.log(avg);
+
+
 }
-
-
 
 
 let numbers = [12, 13, 14, 15, 48, 54, 75, 55];
 let avg = oddAverage(numbers);
 console.log('avaerage of the odd number is: ', avg);
 
-
-// ----------------------------------------------------------------------------------
-
+// -----------------------------------------------------------------------------------------------------------------------------
 // function oddAverage(numbers) {
-//     for (let number of numbers) {
-//         console.log(number);
-//     }
+// // odds is the array thhat contains only the odd numbers
+//     let odds = []
 
+
+//     for (let number of numbers) {
+
+//         if (number % 2 === 0) {
+//             // console.log(number);
+//             odds.push(number);
+//         }
+//     }
+//     console.log(odds);
+//     return odds;
 // }
 
-// let numbers = [12, 13, 14, 15];
+// let numbers = [12, 13, 14, 15, 48, 54, 75, 55];
 // let avg = oddAverage(numbers);
 // console.log('avaerage of the odd number is: ', avg);
+
+// // -----------------------------------------------------------------------------------------------------------------------------
+
+
