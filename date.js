ab452398ed67e472a39e94ca49fa07800cf9dfdd
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 01:08:57 +0800

    Update project

--- date.js
+++ date.js
@@ -1,1 +1,11 @@
 let today = new Date();
+let date = new Date('2062-10-19')
+console.log(date.getMonth());
+console.log(date.getDay());
+let specificDate = new Date(1991, 0, 26)
+console.log(specificDate);
+
+specificDate.setMonth(10);
+console.log(specificDate.toLocaleDateString("en-GB"));
+
+// unix epoc
