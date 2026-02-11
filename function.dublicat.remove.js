151a4601808fe49fc23da13e5914b2892230b6fe
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-11 22:51:28 +0800

    Update project

--- function.dublicat.remove.js
+++ function.dublicat.remove.js
@@ -17,17 +17,23 @@
 
 
 
-// let dubuleName = ['masus', 'rubel', 'romjan', 'forhad', 'shohel', 'masus', 'rubel', 'romjan', 'forhad', 'shohel'];
+let dubuleName = ['masus', 'rubel', 'romjan', 'forhad', 'shohel', 'masus', 'rubel', 'romjan', 'forhad', 'shohel'];
 
-// let number = [1, 22, 2, 33, 3, 55.1, 22, 2, 33, 3, 55]
+let number = [1, 22, 2, 33, 3, 55.1, 22, 2, 33, 3, 55]
 
 
-// function noDublicat(arry){
-//     console.log(arry);
-// }
+function noDublicat(arry) {
+    let unique = [];
+    for (let item of arry) {
+        if (unique.includes(item) === false) {
+            unique.push(item);
+        }
+    }
+    return unique;
+}
 
-// let uniqueArry = noDublicat(dubuleName);
-// console.log(uniqueArry);
+let uniqueArry = noDublicat(dubuleName);
+console.log(uniqueArry);
 
 // ------------------------------------------------------------------------------------------
 
