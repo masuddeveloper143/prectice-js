7d219a92c88eca0370ea7667acd6f5edcfefc548
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-11 22:08:43 +0800

    Update project

---
+++ function.dublicat.remove.js
@@ -0,0 +1,68 @@
+// Array has some  dublicat element fund this is the function 
+
+// let dubuleName = ['masus', 'rubel', 'romjan', 'forhad', 'shohel', 'masus', 'rubel', 'romjan', 'forhad', 'shohel'];
+
+// let number = [1, 22, 2, 33, 3, 55.1, 22, 2, 33, 3, 55]
+
+
+// function noDublicat(arry){
+//     console.log(arry);
+// }
+
+// let uniqueArry = noDublicat(dubuleName);
+// console.log(uniqueArry);
+
+// ------------------------------------------------------------------------------------------
+
+
+
+
+// let dubuleName = ['masus', 'rubel', 'romjan', 'forhad', 'shohel', 'masus', 'rubel', 'romjan', 'forhad', 'shohel'];
+
+// let number = [1, 22, 2, 33, 3, 55.1, 22, 2, 33, 3, 55]
+
+
+// function noDublicat(arry){
+//     console.log(arry);
+// }
+
+// let uniqueArry = noDublicat(dubuleName);
+// console.log(uniqueArry);
+
+// ------------------------------------------------------------------------------------------
+
+
+
+
+
+// let dubuleName = ['masus', 'rubel', 'romjan', 'forhad', 'shohel', 'masus', 'rubel', 'romjan', 'forhad', 'shohel'];
+
+// let number = [1, 22, 2, 33, 3, 55.1, 22, 2, 33, 3, 55]
+
+
+// function noDublicat(arry){
+//     console.log(arry);
+// }
+
+// let uniqueArry = noDublicat(dubuleName);
+// console.log(uniqueArry);
+
+// ------------------------------------------------------------------------------------------
+
+
+
+
+
+// let dubuleName = ['masus', 'rubel', 'romjan', 'forhad', 'shohel', 'masus', 'rubel', 'romjan', 'forhad', 'shohel'];
+
+// let number = [1, 22, 2, 33, 3, 55.1, 22, 2, 33, 3, 55]
+
+
+// function noDublicat(arry){
+//     console.log(arry);
+// }
+
+// let uniqueArry = noDublicat(dubuleName);
+// console.log(uniqueArry);
+
+// ------------------------------------------------------------------------------------------
