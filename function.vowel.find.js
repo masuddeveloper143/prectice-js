4e2138ff782a19c2c1d51d600a994f3776df87f0
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 10:33:26 +0800

    Update project

--- function.vowel.find.js
+++ function.vowel.find.js
@@ -1,2 +1,16 @@
 
 
+function countVowels(str) {
+    let count = 0;
+
+    let vowels = "aeuouAEIOU";
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels("hello"));
+console.log(countVowels("masud rana"));
