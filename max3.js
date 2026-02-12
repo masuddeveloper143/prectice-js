7db84f935e96a307b2076bb2722afc39e8e13ea8
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 17:11:42 +0800

    Update project

---
+++ max3.js
@@ -0,0 +1,66 @@
+let jim = 500;
+let tim = 89;
+let kim = 68;
+
+// if (jim > tim && jim > kim) {
+//     console.log("jim is the ultimate boss");
+// }
+// else if (tim > jim && tim > kim) {
+//     console.log("tim is the boss");
+// }
+
+// else {
+//     console.log("kim is the kardashiyan boss");
+// }
+
+// --------------------------------------------------------
+
+// this is a function  output only number👇
+
+// function maxOfThree(num1, num2, num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         return num1;
+//     }
+//     else if (num2 > num1 && num2 > num3) {
+//         // console.log("num2 is the boss", num2);
+//         return num2;
+
+//     }
+
+//     else {
+//         return num3;
+//     }
+// }
+
+// let number = maxOfThree(11, 55, 44);
+// console.log(number);
+
+
+
+// -----------------------------------------------------------------------------------------------
+// this is a function  output only string and number👇
+
+// function maxOfThree(num1, num2, num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         return num1;
+//     }
+//     else if (num2 > num1 && num2 > num3) {
+//         // console.log("num2 is the boss", num2);
+//         return num2;
+
+//     }
+
+//     else {
+//         return num3;
+//     }
+// }
+
+// let number = maxOfThree(11, 55, 44);
+// console.log(number);
+// ----------------------------------------------------------------------------------------
+
+
+let max = Math.max(12, 44, 55, 66, 77, 88,)
+console.log('maisuing Math.max', max)
+
+// ------------------------------------------------------------------------------------------------
---
+++ tallest.js
@@ -0,0 +1,10 @@
+let heights = [65, 66, 68, 72, 78, 60, 65, 66];
+
+
+function getMax(numbers) {
+    console.log(numbers);
+    return numbers;
+}
+
+let max = getMax(heights);
+console.log('max vaule is', max);
