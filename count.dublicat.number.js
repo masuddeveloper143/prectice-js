b87a6e28d7eb381390d86a0b7521d97917ec0462
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 09:52:37 +0800

    Update project

--- count.dublicat.number.js
+++ count.dublicat.number.js
@@ -1,3 +1,15 @@
-function countNumber(numbers, find){
-    
+function countNumber(numbers, find) {
+    let count = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === find) {
+            count++;
+        }
+    }
+
+    return count;
 }
+
+let result = [1, 5, 9, 5, 9, 6, 8, 8, 8, 9, 1];
+countNumber(result);
+console.log(result);
