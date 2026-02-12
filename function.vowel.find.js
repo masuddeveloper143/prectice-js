e7417f61831b4c7453ed8f632a7ec21e3db561a9
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 10:58:40 +0800

    Update project

--- function.vowel.find.js
+++ function.vowel.find.js
@@ -14,3 +14,23 @@ function countVowels(str) {
 
 console.log(countVowels("hello aeuou"));
 console.log(countVowels("masud "));
+
+
+// --------------------------------------------------------------------------------
+
+
+// 🔥 আরেকটা clean version(lowercase করে)
+
+
+function countVowels(str) {
+    var count = 0;
+    str = str.toLowerCase();
+
+    for (var i = 0; i < str.length; i++) {
+        if ("aeiou".includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
