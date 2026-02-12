82af66f68d914a80b4162420891dd0f87d4520c4
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 20:27:29 +0800

    Update project

--- wood.js
+++ wood.js
@@ -2,19 +2,41 @@
 // tabel-- > 10 cft
 // bed-- > 50 cft
 
-function woodQuantity(chairQuantity, tabelQuantity, bedQuantity) {
-    let perChairWood = 3;
-    let perTabelWood = 10;
-    let perBedWood = 50;
+// function woodQuantity(chairQuantity, tabelQuantity, bedQuantity) {
+//     let perChairWood = 3;
+//     let perTabelWood = 10;
+//     let perBedWood = 50;
 
-    let chairTotalWood = chairQuantity * perChairWood;
-    let tabelTotalWood = tabelQuantity * perTabelWood;
-    let bedTotalWood = bedQuantity * perBedWood;
+//     let chairTotalWood = chairQuantity * perChairWood;
+//     let tabelTotalWood = tabelQuantity * perTabelWood;
+//     let bedTotalWood = bedQuantity * perBedWood;
 
-    let totalWood = chairTotalWood + tabelTotalWood + bedTotalWood;
+//     let totalWood = chairTotalWood + tabelTotalWood + bedTotalWood;
+
+//     return totalWood;
+// }
+
+// let wood = woodQuantity(0, 10, 0);
+// console.log('wood needed', wood);
+
+
+
+// ---------------------------------------------------------------------------------------
+
+
+function woodQuantity2(shortPent, underPent, fullPent) {
+    let perPriceShortPent = 150;
+    let perPriceUnderPent = 50;
+    let perPriceFullPent = 400;
+
+    totalshortPentPrice = shortPent * perPriceShortPent;
+    totalunderPentPrice = underPent * perPriceUnderPent;
+    totalfullPentPentPrice = fullPent * perPriceFullPent;
+
+    totalWood = totalshortPentPrice + totalunderPentPrice + to;
 
     return totalWood;
 }
 
-let wood = woodQuantity(0, 10, 0);
-console.log('wood needed', wood);
+let wood = woodQuantity2(0,0,0,);
+console.log('wood neded', wood)
