33634fe59e84fe07ef667207883d3e8ed0689d15
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 17:20:16 +0800

    Update project

--- tallest.js
+++ tallest.js
@@ -1,9 +1,26 @@
+// let heights = [65, 66, 68, 72, 78, 60, 65, 66];
+
+
+// function getMax(numbers) {
+//     console.log(numbers);
+//     return numbers;
+// }
+
+// let max = getMax(heights);
+// console.log('max vaule is', max);
+
+// -------------------------------------------------------------------------
+
+
+// this is the for loop 
+
+
 let heights = [65, 66, 68, 72, 78, 60, 65, 66];
 
 
 function getMax(numbers) {
-    console.log(numbers);
-    return numbers;
+    for(let number of numbers)
+        console.log(numbers)
 }
 
 let max = getMax(heights);
