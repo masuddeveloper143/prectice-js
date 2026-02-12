103afde9f5afb4d54204fdccec7746a25808b88f
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-12 18:40:24 +0800

    Update project

--- tallest.js
+++ tallest.js
@@ -1,3 +1,50 @@
+
+// this is a function find max value 👇
+
+// let heights = [65, 66, 68, 72, 78, 60, 65, 66, 88];
+
+
+// function getMax(numbers) {
+//     let max = numbers[0];
+//     for (let num of numbers) {
+//         if (num > max) {
+//             max = num;
+//         }
+//     }
+//     return max;
+// }
+
+// let max = getMax(heights);
+// console.log('max vaule is', max);
+
+
+
+// --------------------------------------------------------------------------------------------
+
+// tis is a function find min value 👇
+
+let heights = [65, 66, 68, 72, 78, 60, 65, 66, 88];
+
+function getMax2(numbers) {
+    let max = numbers[0];
+    for (let num of numbers) {
+        if (num < max) {
+            max = num;
+        }
+    }
+    return max;
+}
+
+
+
+let max = getMax2(heights);
+console.log('min value is the number', max);
+
+
+// ---------------------------------------------------------------------------------------
+
+
+
 // let heights = [65, 66, 68, 72, 78, 60, 65, 66];
 
 
@@ -12,16 +59,43 @@
 // -------------------------------------------------------------------------
 
 
-// this is the for loop 
+
+// -------------------------------------------------------------------------
 
 
-let heights = [65, 66, 68, 72, 78, 60, 65, 66];
+// this is the for loop
 
 
-function getMax(numbers) {
-    for(let number of numbers)
-        console.log(numbers)
-}
+// let heights = [65, 66, 68, 72, 78, 60, 65, 66];
 
-let max = getMax(heights);
-console.log('max vaule is', max);
+
+// function getMax(numbers) {
+//     for (let num of numbers) {
+//         console.log(num);
+
+//     }
+// }
+
+// let max = getMax(heights);
+// console.log('max vaule is', max);
+
+
+// ---------------------------------------------------------------------------------------------
+
+
+
+
+// function getMax(numbers) {
+//     for (let num of numbers) {
+//         console.log(num);
+//     }
+
+// }
+
+
+// let heights = [65, 66, 68, 72, 78, 60, 65, 66];
+// let max = getMax(heights);
+// console.log('max vaule is', max);
+
+// // -------------------------------------------------------------------------
+
