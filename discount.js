2dede2b3917b1c54e5d198be8cd82d9f983a39e0
Masud <md.masudreja198@gmail.com>
2026-02-13 11:53:19 +0800

    Update project

---
+++ discount.js
@@ -0,0 +1,11 @@
+// uoto -->100
+// more than 101-200: -->90
+// more than 200: --> 70
+
+
+function discountedPrice(quantity){
+    if(if quantity <= 100){
+        let total = quantity*100;
+        return total;
+    }
+}
