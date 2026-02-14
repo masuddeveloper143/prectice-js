4b91c224a0570f64444538a7b7c1bb0c2ae1b3e9
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-14 10:23:00 +0800

    Update project

--- function.validetion.js
+++ function.validetion.js
@@ -5,3 +5,31 @@ function multiply(num1, num2) {
 
 let result = multiply(4, 4)
 console.log(result);
+
+
+// -----------------------------------------------------------------
+
+
+function multiply(num1, num2) {
+
+    if (typeof num1 !== 'number') {
+        return 'please provide your number '
+    }
+    let mult = num1 * num2;
+    return mult;
+}
+
+let result = multiply('5', 'seven')
+console.log(result);
+
+
+// -----------------------------------------------------------------------
+
+
+function multiply(num1, num2) {
+    let mult = num1 * num2;
+    return mult;
+}
+
+let result = multiply(4, 4)
+console.log(result);
