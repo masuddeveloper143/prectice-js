c78b02daedca89c31774abf707c06d4be1aafc8b
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-14 17:56:09 +0800

    Update project

--- smallest_name.js
+++ smallest_name.js
@@ -1,1 +1,11 @@
-let friends = 
+let friends = ["Rahim""Karim""Al""Sumi""Hasan"];
+
+let smallest = friends[0];
+
+for (let i = 1; i < friends.length; i++) {
+    if (friends[i].length < smallest.length) {
+        smallest = friends[i];
+    }
+}
+
+console.log("smallest name: ", smallest);
