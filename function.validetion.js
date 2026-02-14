d8eb3cc151a706900b32b9a1b1c50dac39ec1b81
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-14 10:50:11 +0800

    Update project

--- function.validetion.js
+++ function.validetion.js
@@ -1,10 +1,10 @@
-function multiply(num1, num2) {
-    let mult = num1 * num2;
-    return mult;
-}
+// function multiply(num1, num2) {
+//     let mult = num1 * num2;
+//     return mult;
+// }
 
-let result = multiply(4, 4)
-console.log(result);
+// let result = multiply(4, 4)
+// console.log(result);
 
 
 // -----------------------------------------------------------------
@@ -12,24 +12,31 @@ console.log(result);
 
 function multiply(num1, num2) {
 
-    if (typeof num1 !== 'number') {
-        return 'please provide your number '
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'please provide your number'
     }
     let mult = num1 * num2;
     return mult;
 }
 
-let result = multiply('5', 'seven')
-console.log(result);
+// let result = multiply('5', 'seven')
+// console.log(result);
 
 
 // -----------------------------------------------------------------------
 
 
-function multiply(num1, num2) {
-    let mult = num1 * num2;
-    return mult;
+function fulleName(first, second) {
+    if (typeof first !== 'string') {
+        return 'first name should be a string'
+    }
+
+    else if(typeof second !== 'string'){
+        return 'second Name should be a string'
+    }
+    let full = first + ' ' + second;
+    return full;
 }
 
-let result = multiply(4, 4)
-console.log(result);
+let result = fulleName(4, 7)
+console.log(result)
