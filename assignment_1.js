5619077e377e0a02f83b2a414b6b70612ac5094f
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-15 11:55:17 +0800

    Update project

--- assignment_1.js
+++ assignment_1.js
@@ -4,9 +4,14 @@ function calculateVAT(price) {
         return 'invalid';
     }
 
-    else if (price <= 0)
+    else if (price <= 0) {
         return 'invalid';
+    }
+
+    let vat = price * 0.075;
+    return vat;
+
 }
 
-let result = calculateVAT(0);
+let result = calculateVAT(1);
 console.log(result);
