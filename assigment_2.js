514004cdd19684afd0b6094736c0c6ab4cc16c79
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-15 12:36:57 +0800

    Update project

--- assigment_2.js
+++ assigment_2.js
@@ -1,2 +1,20 @@
 
 
+function validContact(contact) {
+    if (typeof contact !== 'string') {
+        return 'invalid';
+    }
+    if (contact.length !== 11) {
+        return false;
+    }
+    if (!contact.startsWith("01")) {
+        return false;
+    }
+    if (contact.includes(" ")) {
+        return false;
+    }
+    return true;
+}
+
+let result = validContact('01567890989');
+console.log(result);
