7d9254c72d129153939181fb533dc68d35062beb
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-16 16:09:22 +0800

    Update project

--- assignment.5.js
+++ assignment.5.js
@@ -6,7 +6,29 @@ function calculateSleepTime(times) {
 
     let totalSeconds = 0;
 
-    for (let time of times){
-        if(!)
+    for (let time of times) {
+        if (typeof time !== 'number') {
+            return "invalid";
+        }
+
+        totalSeconds += time;
+    }
+
+    let hour = Math.floor(totalSeconds / 3600);
+    let remainingSecond = totalSeconds % 3600;
+
+    let minute = Math.floor(totalSeconds / 60);
+    let remainingSecond = totalSeconds % 60;
+
+    return {
+        hour: hour,
+        minute: minute,
+        second: second
     }
 }
+
+
+
+
+
+
