c0efee5227245f3b97864a6b86b7502f951c23f7
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-16 15:54:35 +0800

    Update project

---
+++ assignment.5.js
@@ -0,0 +1,12 @@
+
+function calculateSleepTime(times) {
+    if (!Array.isArray) {
+        return "invalid";
+    }
+
+    let totalSeconds = 0;
+
+    for (let time of times){
+        if(!)
+    }
+}
